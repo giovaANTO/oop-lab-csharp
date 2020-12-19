b1ca5e5b34ad1023071ecf9921610614669ef47f
Giova <giovanni.antonioni2@studio.unibo.it>
2020-12-19 03:15:48 +0100

    Complete Exercise 2

--- 2-Indexers/IMap2D.cs
+++ 2-Indexers/IMap2D.cs
@@ -1,10 +1,10 @@
+using System;
+using System.Collections.Generic;
+
 namespace Indexers
 {
-    using System;
-    using System.Collections.Generic;
-
     /// <summary>
-    /// A bidimentional map.
+    ///     A bidimentional map.
     /// </summary>
     /// <typeparam name="TKey1">the type of the first key.</typeparam>
     /// <typeparam name="TKey2">the type of the second key.</typeparam>
@@ -12,12 +12,12 @@ namespace Indexers
     public interface IMap2D<TKey1, TKey2, TValue> : IEquatable<IMap2D<TKey1, TKey2, TValue>>
     {
         /// <summary>
-        /// Gets the number of values in the map.
+        ///     Gets the number of values in the map.
         /// </summary>
         int NumberOfElements { get; }
 
         /// <summary>
-        /// The indexer used to access the value of the map.
+        ///     The indexer used to access the value of the map.
         /// </summary>
         /// <param name="key1">the first key.</param>
         /// <param name="key2">the second key.</param>
@@ -25,31 +25,33 @@ namespace Indexers
         TValue this[TKey1 key1, TKey2 key2] { get; set; }
 
         /// <inheritdoc cref="object.ToString" />
-        /// <remarks><para>The string is a textual representation of the bidimentional map.</para></remarks>
+        /// <remarks>
+        ///     <para>The string is a textual representation of the bidimentional map.</para>
+        /// </remarks>
         string ToString();
 
         /// <summary>
-        /// Get the row of the map corresponding to the given first key.
+        ///     Get the row of the map corresponding to the given first key.
         /// </summary>
         /// <param name="key1">the first key.</param>
         /// <returns>the row as a list of tuples.</returns>
         IList<Tuple<TKey2, TValue>> GetRow(TKey1 key1);
 
         /// <summary>
-        /// Get the column of the map corresponding to the given second key.
+        ///     Get the column of the map corresponding to the given second key.
         /// </summary>
         /// <param name="key2">the second key.</param>
         /// <returns>the column as list of tuples.</returns>
         IList<Tuple<TKey1, TValue>> GetColumn(TKey2 key2);
 
         /// <summary>
-        /// Get all the values of the map as tuples.
+        ///     Get all the values of the map as tuples.
         /// </summary>
         /// <returns>the values of the map.</returns>
         IList<Tuple<TKey1, TKey2, TValue>> GetElements();
 
         /// <summary>
-        /// Fill the map with the values from the given enumerations.
+        ///     Fill the map with the values from the given enumerations.
         /// </summary>
         /// <param name="keys1">all the row keys.</param>
         /// <param name="keys2">all the column keys.</param>
--- 2-Indexers/Map2D.cs
+++ 2-Indexers/Map2D.cs
@@ -1,79 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace Indexers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Linq;
-
     /// <inheritdoc cref="IMap2D{TKey1,TKey2,TValue}" />
     public class Map2D<TKey1, TKey2, TValue> : IMap2D<TKey1, TKey2, TValue>
     {
+        private Dictionary<Tuple<TKey1, TKey2>, TValue> map = new Dictionary<Tuple<TKey1, TKey2>, TValue>();
+
+        public static IEqualityComparer<Map2D<TKey1, TKey2, TValue>> MapComparer { get; } = new MapEqualityComparer();
+
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.NumberOfElements" />
-        public int NumberOfElements
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public int NumberOfElements => map.Count;
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.this" />
         public TValue this[TKey1 key1, TKey2 key2]
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get => map[new Tuple<TKey1, TKey2>(key1, key2)];
+            set => map[new Tuple<TKey1, TKey2>(key1, key2)] = value;
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetRow(TKey1)" />
         public IList<Tuple<TKey2, TValue>> GetRow(TKey1 key1)
         {
-            throw new NotImplementedException();
+            return map.Keys
+                .Where(tup => tup.Item1.Equals(key1))
+                .Select(t => new Tuple<TKey2, TValue>(t.Item2, map[t]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetColumn(TKey2)" />
         public IList<Tuple<TKey1, TValue>> GetColumn(TKey2 key2)
         {
-            throw new NotImplementedException();
+            return map.Keys
+                .Where(tup => tup.Item2.Equals(key2))
+                .Select(t => new Tuple<TKey1, TValue>(t.Item1, map[t]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetElements" />
         public IList<Tuple<TKey1, TKey2, TValue>> GetElements()
         {
-            throw new NotImplementedException();
+            return map.Keys
+                .Select(tup => new Tuple<TKey1, TKey2, TValue>(tup.Item1, tup.Item2, map[tup]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.Fill(IEnumerable{TKey1}, IEnumerable{TKey2}, Func{TKey1, TKey2, TValue})" />
         public void Fill(IEnumerable<TKey1> keys1, IEnumerable<TKey2> keys2, Func<TKey1, TKey2, TValue> generator)
         {
-            throw new NotImplementedException();
+            keys1.ToList()
+                .ForEach(k1 => keys2.ToList()
+                    .ForEach( k2 => this.map[Tuple.Create<TKey1, TKey2>(k1,k2)] = generator(k1, k2)));
         }
 
-        /// <inheritdoc cref="IEquatable{T}.Equals(T)" />
         public bool Equals(IMap2D<TKey1, TKey2, TValue> other)
         {
-            // TODO: improve
-            return base.Equals(other);
+            return MapComparer.Equals(other);
         }
 
-        /// <inheritdoc cref="object.Equals(object?)" />
-        public override bool Equals(object obj)
+        private sealed class MapEqualityComparer : IEqualityComparer<Map2D<TKey1, TKey2, TValue>>
         {
-            // TODO: improve
-            return base.Equals(obj);
+            public bool Equals(Map2D<TKey1, TKey2, TValue> x, Map2D<TKey1, TKey2, TValue> y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return Equals(x.map, y.map);
+            }
+
+            public int GetHashCode(Map2D<TKey1, TKey2, TValue> obj)
+            {
+                return (obj.map != null ? obj.map.GetHashCode() : 0);
+            }
         }
 
-        /// <inheritdoc cref="object.GetHashCode"/>
-        public override int GetHashCode()
+        public string ToString()
         {
-            // TODO: improve
-            return base.GetHashCode();
-        }
-
-        /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.ToString"/>
-        public override string ToString()
-        {
-            // TODO: improve
-            return base.ToString();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var keyValuePair in map)
+            {
+                stringBuilder.Append(String.Format("<K1 : {0}, K2 : {1}, Val : {2}", keyValuePair.Key.Item1, keyValuePair.Key.Item2, keyValuePair.Value));
+            }
+            return stringBuilder.ToString();
         }
     }
 }
