bec115c806a69b08c0136ac53246c46730bd289d
Giova <giovanni.antonioni2@studio.unibo.it>
2020-12-24 00:51:21 +0100

    Complete exercise 4

--- 4-ExtensionMethods/Complex.cs
+++ 4-ExtensionMethods/Complex.cs
@@ -1,15 +1,18 @@
+using System;
+using System.Collections.Generic;
+
 namespace ExtensionMethods
 {
-    using System;
-
-    /// <inheritdoc cref="IComplex"/>
+    /// <inheritdoc cref="IComplex" />
     public class Complex : IComplex
     {
-        private readonly double re;
+        private const double Tolerance = 1E-7;
+
         private readonly double im;
+        private readonly double re;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Complex"/> class.
+        ///     Initializes a new instance of the <see cref="Complex" /> class.
         /// </summary>
         /// <param name="re">the real part.</param>
         /// <param name="im">the imaginary part.</param>
@@ -19,67 +22,45 @@ namespace ExtensionMethods
             this.im = im;
         }
 
-        /// <inheritdoc cref="IComplex.Real"/>
-        public double Real
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        /// <inheritdoc cref="IComplex.Real" />
+        public double Real => this.re;
 
-        /// <inheritdoc cref="IComplex.Imaginary"/>
-        public double Imaginary
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        /// <inheritdoc cref="IComplex.Imaginary" />
+        public double Imaginary => this.im;
 
-        /// <inheritdoc cref="IComplex.Modulus"/>
-        public double Modulus
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        /// <inheritdoc cref="IComplex.Modulus" />
+        public double Modulus => Math.Sqrt(Math.Pow(this.Real,2) + Math.Pow(this.Imaginary, 2));
 
-        /// <inheritdoc cref="IComplex.Phase"/>
-        public double Phase
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        /// <inheritdoc cref="IComplex.Phase" />
+        public double Phase => Math.Atan2(this.Real, this.Imaginary);
 
-        /// <inheritdoc cref="IComplex.ToString"/>
+        /// <inheritdoc cref="IComplex.ToString" />
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            string sign = this.Imaginary >= 0 ? "+" : "-";
+            return String.Format("{0}{1}i{2}",this.Real,sign,Math.Abs(this.Imaginary));
         }
 
-        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
+        /// <inheritdoc cref="IEquatable{T}.Equals(T)" />
         public bool Equals(IComplex other)
         {
-            throw new System.NotImplementedException();
+            return other != null
+                   && Math.Abs(other.Real - Real) < Tolerance
+                   && Math.Abs(other.Imaginary - Imaginary) < Tolerance;
         }
 
-        /// <inheritdoc cref="object.Equals(object?)"/>
         public override bool Equals(object obj)
         {
-            // TODO improve
-            return base.Equals(obj);
+            if (obj is Complex complex)
+            {
+                return this.Equals(complex);
+            }
+            return false;
         }
 
-        /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
-            // TODO improve
-            return base.GetHashCode();
+            return HashCode.Combine(re, im);
         }
     }
 }
--- 4-ExtensionMethods/ComplexExtensions.cs
+++ 4-ExtensionMethods/ComplexExtensions.cs
@@ -3,84 +3,92 @@ using System;
 namespace ExtensionMethods
 {
     /// <summary>
-    /// The static class declares extension methods for complex numbers.
+    ///     The static class declares extension methods for complex numbers.
     /// </summary>
     public static class ComplexExtensions
     {
         /// <summary>
-        /// Add two complex numbers.
+        ///     Add two complex numbers.
         /// </summary>
         /// <param name="c1">the first operand.</param>
         /// <param name="c2">the second operand.</param>
         /// <returns>the sum.</returns>
         public static IComplex Add(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
         }
 
         /// <summary>
-        /// Substract <paramref name="c2"/> from <paramref name="c1"/>.
+        ///     Substract <paramref name="c2" /> from <paramref name="c1" />.
         /// </summary>
         /// <param name="c1">the first operand.</param>
         /// <param name="c2">the second operand.</param>
         /// <returns>the difference.</returns>
         public static IComplex Subtract(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
         }
 
         /// <summary>
-        /// Multiply two complex numbers.
+        ///     Multiply two complex numbers.
         /// </summary>
         /// <param name="c1">the first operand.</param>
         /// <param name="c2">the second operand.</param>
         /// <returns>the product.</returns>
         public static IComplex Multiply(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(
+                (c1.Real * c2.Real) - (c1.Imaginary * c2.Imaginary),
+                (c1.Real * c2.Imaginary) + (c1.Imaginary * c2.Real)
+                );
         }
 
         /// <summary>
-        /// Divide two complex numbers.
+        ///     Divide two complex numbers.
         /// </summary>
         /// <param name="c1">the first operand.</param>
         /// <param name="c2">the second operand.</param>
         /// <returns>the quotient.</returns>
         public static IComplex Divide(this IComplex c1, IComplex c2)
         {
-            throw new NotImplementedException();
+            return new Complex(
+                ((c1.Real * c2.Real) + (c1.Imaginary * c2.Imaginary)) / (Math.Pow(c2.Real, 2) + Math.Pow(c2.Imaginary, 2)),
+                ((c1.Imaginary * c2.Real) - (c1.Real * c2.Imaginary)) / (Math.Pow(c2.Real, 2) + Math.Pow(c2.Imaginary, 2))
+                );
         }
 
         /// <summary>
-        /// Get the complex conjugate of a complex number.
+        ///     Get the complex conjugate of a complex number.
         /// </summary>
         /// <remarks>
-        /// <para>
-        /// The complex conjugate of a complex number is the number with an equal real part
-        /// and an imaginary part equal in magnitude, but opposite in sign.
-        /// </para>
+        ///     <para>
+        ///         The complex conjugate of a complex number is the number with an equal real part
+        ///         and an imaginary part equal in magnitude, but opposite in sign.
+        ///     </para>
         /// </remarks>
         /// <param name="c1">the complex operand.</param>
         /// <returns>the complex conjugate.</returns>
         public static IComplex Conjugate(this IComplex c1)
         {
-            throw new NotImplementedException();
+            return new Complex(c1.Real, -1 * c1.Imaginary);
         }
 
         /// <summary>
-        /// Get the reciprocal of a complex number.
+        ///     Get the reciprocal of a complex number.
         /// </summary>
         /// <remarks>
-        /// <para>
-        /// The multiplicative inverse (or reciprocal) of a complex number is a number,
-        /// that when multiplied with that complex number, gives an answer of 1.
-        /// </para>
+        ///     <para>
+        ///         The multiplicative inverse (or reciprocal) of a complex number is a number,
+        ///         that when multiplied with that complex number, gives an answer of 1.
+        ///     </para>
         /// </remarks>
         /// <param name="c1">the complex operand.</param>
         /// <returns>the complex reciprocal.</returns>
         public static IComplex Reciprocal(this IComplex c1)
         {
-            throw new NotImplementedException();
+            IComplex conjugate = c1.Conjugate();
+            double mulFactor = 1.0 / Math.Pow(c1.Modulus, 2);
+            return new Complex(conjugate.Real * mulFactor, conjugate.Imaginary * mulFactor);
         }
     }
 }
