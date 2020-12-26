08dae3ab90ab82efad7e06e6d844536482905e18
Giova <giovanni.antonioni2@studio.unibo.it>
2020-12-26 22:21:20 +0100

    Complete exercise 6

--- 6-Iterators/Program.cs
+++ 6-Iterators/Program.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Iterators
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
     /// <summary>
-    /// The runnable entrypoint of the exercise.
+    ///     The runnable entrypoint of the exercise.
     /// </summary>
     public class Program
     {
@@ -13,45 +13,36 @@ namespace Iterators
         public static void Main()
         {
             const int len = 50;
-            int?[] numbers = new int?[len];
-            Random rand = new Random();
-            for (int i = 0; i < len; i++)
-            {
+            var numbers = new int?[len];
+            var rand = new Random();
+            for (var i = 0; i < len; i++)
                 if (rand.NextDouble() > 0.2)
-                {
                     numbers[i] = rand.Next(len);
-                }
-            }
 
             // TODO rewrite using methods from Java8StreamOperations
             IDictionary<int, int> occurrences = numbers
-                .Select(optN => {
-                    Console.Write(optN.ToString() + ",");
+                .Select(optN =>
+                {
+                    Console.Write(optN + ",");
                     return optN;
                 })
                 .Skip(1)
                 .Take(len - 2)
                 .Where(optN => optN.HasValue)
                 .Select(optN => optN.Value)
-                .Aggregate(new Dictionary<int, int>(), (d, n) => {
+                .Aggregate(new Dictionary<int, int>(), (d, n) =>
+                {
                     if (!d.ContainsKey(n))
-                    {
                         d[n] = 1;
-                    }
                     else
-                    {
                         d[n]++;
-                    }
 
                     return d;
                 });
 
             Console.WriteLine();
 
-            foreach (KeyValuePair<int, int> kv in occurrences)
-            {
-                Console.WriteLine(kv);
-            }
+            foreach (var kv in occurrences) Console.WriteLine(kv);
         }
     }
 }
